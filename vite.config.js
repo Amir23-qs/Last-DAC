eb95e386ffe2b0515ff48fa62a3bf20f878debc1
Amir Ashraf <amir.ashraf20192019@gmail.com>
2026-06-26 00:01:33 +0300

    opencode

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  base: '../',
-=======
-  base: '/',
->>>>>>> b704ea1d2e804252aeaf6c6ab86c200385875e6c
+  base: '/Last-DAC/',
   plugins: [react()],
 })
